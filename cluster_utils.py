24a89bc373b4f8e8c6fb0111ba52d1c8253f1a81
Emma Boland <emmomp@bas.ac.uk>
2022-11-14 17:02:22 +0000

    Sorting out average profile calc and plotting profile matching

--- cluster_utils.py
+++ cluster_utils.py
@@ -80,7 +80,7 @@ def retrieve_profiles(timeRange = slice('1965-01', '1994-12'), levSel=slice(100,
     except:
         pass
 
-    with dask.config.set(**{'array.slicing.split_large_chunks': True}):
+    with dask.config.set(**{'array.slicing.split_large_chunks': False}):
         
         data = dataRaw.sel(lev=levSel, time=timeRange)
         data = data.where(data.lat < maxLat, drop=True)
@@ -98,20 +98,20 @@ def retrieve_profiles(timeRange = slice('1965-01', '1994-12'), levSel=slice(100,
 
 
 def random_sample(data, N):
-  """
-  Returns a random sample of the data. The input array should have dimensions 
-  time, n and lev. Returns an array with N*len(data['time']) profiles, evenly sampled
-  over time. Use data.persist() or data.compute() after this to avoid resampling for every computation
-  
-  :param data: the temperature-depth profiles, coordinates ['time', 'n', 'lev']
-  :param N: the number of samples from each time 
-  :return: xarray.DataArray with coordinates ['N', 'lev']
-  """
-  rng = np.random.default_rng()
-  def func(arr):
-    return rng.choice(arr, N, replace=False)
+    """
+      Returns a random sample of the data. The input array should have dimensions 
+      time, n and lev. Returns an array with N*len(data['time']) profiles, evenly sampled
+      over time. Use data.persist() or data.compute() after this to avoid resampling for every computation
 
-  result = xr.apply_ufunc(
+      :param data: the temperature-depth profiles, coordinates ['time', 'n', 'lev']
+      :param N: the number of samples from each time 
+      :return: xarray.DataArray with coordinates ['N', 'lev']
+    """
+    rng = np.random.default_rng()
+    def func(arr):
+        return rng.choice(arr, N, replace=False)
+
+    result = xr.apply_ufunc(
         func,
         data.chunk(dict(n=-1)),
         input_core_dims=[['n', 'lev']],
@@ -125,129 +125,128 @@ def random_sample(data, N):
         
     )
   
-  result = result.stack(N=('M', 'time'))
-  result = result.transpose('N', 'lev')
-  return result
+    result = result.stack(N=('M', 'time'))
+    result = result.transpose('N', 'lev')
+    return result
 
 
 def normalise_data(data, dim=-1, return_inv=False):
-  if return_inv:
-    mean = data.mean(dim)
-    std = data.std(dim)
-    return ((data - mean) / std, mean, std)
-  else:
-    return (data - data.mean(dim)) / data.std(dim)
+    if return_inv:
+        mean = data.mean(dim)
+        std = data.std(dim)
+        return ((data - mean) / std, mean, std)
+    else:
+        return (data - data.mean(dim)) / data.std(dim)
 
 def train_pca(data_sampled, n_components=3):
-  """
-  data_sampled (N, lev) returns pca object
-  """
-  
-  pca = PCA(n_components)
-  arr = data_sampled.values
-  pca = pca.fit(arr)
-  return pca
+    """
+      data_sampled (N, lev) returns pca object
+    """
+
+    pca = PCA(n_components)
+    arr = data_sampled.values
+    pca = pca.fit(arr)
+    return pca
 
 def pca_transform(data, pca):
-  """
-  Applies a transformation into PCA space
-  
-  """
+    """
+      Applies a transformation into PCA space
+    """
 
-  lev_size = data.sizes['lev']
-  n_comp = pca.n_components
-  
-  def func(arr):
-    arr_r = np.reshape(arr, (-1, lev_size))
-    
-    inds = np.isnan(arr_r)
-    arr_r[inds] = 0
-    
-    out = pca.transform(arr_r)
-    out[inds[..., 0:n_comp]] = np.nan
-    out_sizes = np.shape(arr[..., 0:n_comp])
-    
-    out = np.reshape(out, out_sizes)
-    return out
+    lev_size = data.sizes['lev']
+    n_comp = pca.n_components
 
-  result = xr.apply_ufunc(
-        func,
-        data,
-        input_core_dims=[['lev']],
-        output_core_dims=[['pca_comp']],
-        dask='parallelized',
-        output_dtypes=('float64',),
-        vectorize=False,
-        dask_gufunc_kwargs={
-          'output_sizes' : {'pca_comp' : n_comp},
-          'allow_rechunk' : True
-        }
-        
-    )
-  
-  return result
+    def func(arr):
+        arr_r = np.reshape(arr, (-1, lev_size))
+
+        inds = np.isnan(arr_r)
+        arr_r[inds] = 0
+
+        out = pca.transform(arr_r)
+        out[inds[..., 0:n_comp]] = np.nan
+        out_sizes = np.shape(arr[..., 0:n_comp])
+
+        out = np.reshape(out, out_sizes)
+        return out
+
+    result = xr.apply_ufunc(
+            func,
+            data,
+            input_core_dims=[['lev']],
+            output_core_dims=[['pca_comp']],
+            dask='parallelized',
+            output_dtypes=('float64',),
+            vectorize=False,
+            dask_gufunc_kwargs={
+              'output_sizes' : {'pca_comp' : n_comp},
+              'allow_rechunk' : True
+            }
+
+        )
+
+    return result
 
 def train_gmm(data_trans, K):
-  """
-  Trains a GMM on the data
-  """
-  gmm = GaussianMixture(K)
-  gmm.fit(data_trans.values)
-  return gmm
+    """
+    Trains a GMM on the data
+    """
+    gmm = GaussianMixture(K)
+    gmm.fit(data_trans.values)
+    return gmm
 
 def gmm_classify(data_trans, gmm):
   
-  """
-  Replace the nan with -1
-  """
+    """
+    Replace the nan with -1
+    """
 
-  pca_size = data_trans.sizes['pca_comp']
-  def func(arr):
-    arr_r = np.reshape(arr, (-1, pca_size))
-    
-    inds = np.isnan(arr_r) 
-    arr_r[inds] = 0
-    
-    out = gmm.predict(arr_r)
-    out_sizes = np.shape(arr[..., 0])
-    out[inds[:, 0]] = -1 #replaces the nan values with -1
-    out = np.reshape(out, out_sizes)
-    return out
+    pca_size = data_trans.sizes['pca_comp']
+    def func(arr):
+        arr_r = np.reshape(arr, (-1, pca_size))
 
-  result = xr.apply_ufunc(
-        func,
-        data_trans,
-        input_core_dims=[['pca_comp']],
-        output_core_dims=[[]],
-        dask='parallelized',
-        output_dtypes=('int',),
-        vectorize=False,
-    )
-  
-  return result
+        inds = np.isnan(arr_r) 
+        arr_r[inds] = 0
+
+        out = gmm.predict(arr_r)
+        out_sizes = np.shape(arr[..., 0])
+        out[inds[:, 0]] = -1 #replaces the nan values with -1
+        out = np.reshape(out, out_sizes)
+        return out
+
+    result = xr.apply_ufunc(
+            func,
+            data_trans,
+            input_core_dims=[['pca_comp']],
+            output_core_dims=[[]],
+            dask='parallelized',
+            output_dtypes=('int',),
+            vectorize=False,
+        )
+
+    return result
 
 def gmm_prob(data_trans, gmm):
   
-  """
-  Replace the nan with -1
-  """
+    """
+    Replace the nan with -1
+    """
 
-  pca_size = data_trans.sizes['pca_comp']
-  gmm_size = gmm.n_components
-  def func(arr):
-    arr_r = np.reshape(arr, (-1, pca_size))
-    
-    inds = np.isnan(arr_r) 
-    arr_r[inds] = 0
-    
-    out = gmm.predict_proba(arr_r)
-    out_sizes = list(np.shape(arr))
-    out_sizes[-1] = gmm_size
-    out[inds[:, 0]] = np.nan
-    out = np.reshape(out, out_sizes)
-    return out
+    pca_size = data_trans.sizes['pca_comp']
+    gmm_size = gmm.n_components
+    def func(arr):
+        arr_r = np.reshape(arr, (-1, pca_size))
 
-  result = xr.apply_ufunc(
+        inds = np.isnan(arr_r) 
+        arr_r[inds] = 0
+
+        out = gmm.predict_proba(arr_r)
+        out_sizes = list(np.shape(arr))
+        out_sizes[-1] = gmm_size
+        out[inds[:, 0]] = np.nan
+        out = np.reshape(out, out_sizes)
+        return out
+
+    result = xr.apply_ufunc(
         func,
         data_trans,
         input_core_dims=[['pca_comp']],
@@ -260,82 +259,72 @@ def gmm_prob(data_trans, gmm):
         }
     )
   
-  return result
-  
+    return result
+
   
 def gmm_score_samples(data_trans, gmm):
   
-  """
-  Replace the nan with -1
-  """
+    """
+    Replace the nan with -1
+    """
 
-  pca_size = data_trans.sizes['pca_comp']
-  gmm_size = gmm.n_components
-  def func(arr):
-    arr_r = np.reshape(arr, (-1, pca_size))
-    
-    inds = np.isnan(arr_r) 
-    arr_r[inds] = 0
-    
-    out = gmm.score_samples(arr_r)
-    out_sizes = np.shape(arr)
-    out_sizes = out_sizes[0:-1]
-    out[inds[:, 0]] = np.nan
-    out = np.reshape(out, out_sizes)
-    return out
+    pca_size = data_trans.sizes['pca_comp']
+    gmm_size = gmm.n_components
+    def func(arr):
+        arr_r = np.reshape(arr, (-1, pca_size))
 
-  result = xr.apply_ufunc(
-        func,
-        data_trans,
-        input_core_dims=[['pca_comp']],
-        output_core_dims=[[]],
-        dask='parallelized',
-        output_dtypes=('float64',),
-        vectorize=False,
-        dask_gufunc_kwargs={
-            'output_sizes' : {}
-        }
-    )
-  
-  return result
+        inds = np.isnan(arr_r) 
+        arr_r[inds] = 0
+
+        out = gmm.score_samples(arr_r)
+        out_sizes = np.shape(arr)
+        out_sizes = out_sizes[0:-1]
+        out[inds[:, 0]] = np.nan
+        out = np.reshape(out, out_sizes)
+        return out
+
+    result = xr.apply_ufunc(
+            func,
+            data_trans,
+            input_core_dims=[['pca_comp']],
+            output_core_dims=[[]],
+            dask='parallelized',
+            output_dtypes=('float64',),
+            vectorize=False,
+            dask_gufunc_kwargs={
+                'output_sizes' : {}
+            }
+        )
+
+    return result
 
 def generate_trainingset(timeRange = slice('1965-01', '1994-12'), mask=None, options={},n_components=3,N=7000):
     # Get profiles from googleapi CMIP6 data store
-    data = retrieve_profiles(timeRange,mask,options)
+    data = retrieve_profiles(timeRange=timeRange,mask=mask,options=options)
     # Subset by chooseing N random profiles per month in the Southern Ocean
-    data_sampled = flt.random_sample(data, N).compute()
+    data_sampled = random_sample(data, N).compute()
     # Normalise the samples
-    data_sampled = flt.normalise_data(data_sampled, 'N') 
+    data_sampled = normalise_data(data_sampled, 'N') 
     #Fit PCA model  
-    pca = flt.train_pca(data_sampled, n_components)   
+    pca = train_pca(data_sampled, n_components)   
     # Transform training set to PCA space
-    data_trans = flt.pca_transform(data_sampled, pca).compute() 
+    data_trans = pca_transform(data_sampled, pca)
     return data_trans,pca
 
-def generate_fullset(timeRange = slice('1965-01', '1994-12'), mask=None, options={},n_components=3,pca=None):
-    # Get profiles from googleapi CMIP6 data store
-    data = retrieve_profiles(timeRange,mask,options)
-    data = data.chunk({'time': data.sizes['time'], 'n': 1024}).compute()
-    # Normalise the samples
-    data_sampled = flt.normalise_data(data_sampled, 'N') 
-    # Transform training set to PCA space using provided PCA model
-    data_trans = flt.pca_transform(data_sampled, pca).compute() 
-    return data_trans
-
 def count_area(data_classes, K):
-  """
-  Counts the number of assignments for each class, weighted by the latitude. The result is proportional to the total ocean surface area
-  of profiles assigned to each class
-  """
-  lats = data_classes['lat'].values
-  cos_lats = np.cos(lats * np.pi / 180)
-  def func(arr):
-    out = np.zeros(K)
-    for i in range(K):
-      out[i] = np.sum(cos_lats * (arr==i))
+    """
+    Counts the number of assignments for each class, weighted by the latitude. The result is proportional to the total ocean surface area
+    of profiles assigned to each class
+    """
+    lats = data_classes['lat'].values
+    cos_lats = np.cos(lats * np.pi / 180)
+    def func(arr):
+        out = np.zeros(K)
+        for i in range(K):
+            out[i] = np.sum(cos_lats * (arr==i))
     return out
 
-  result = xr.apply_ufunc(
+    result = xr.apply_ufunc(
         func,
         data_classes,
         input_core_dims=[['n']],
@@ -348,18 +337,18 @@ def count_area(data_classes, K):
         }
     )
   
-  return result
+    return result
 
 def mean_lat(data_classes, K):
-  lats = data_classes['lat'].values
-  # cos_lats = np.cos(lats * np.pi / 180)
-  def func(arr):
-    out = np.zeros(K)
-    for i in range(K):
-      out[i] = np.mean(lats * (arr==i))
+    lats = data_classes['lat'].values
+    # cos_lats = np.cos(lats * np.pi / 180)
+    def func(arr):
+        out = np.zeros(K)
+        for i in range(K):
+            out[i] = np.mean(lats * (arr==i))
     return out
 
-  result = xr.apply_ufunc(
+    result = xr.apply_ufunc(
         func,
         data_classes,
         input_core_dims=[['n']],
@@ -371,93 +360,94 @@ def mean_lat(data_classes, K):
             'output_sizes' : {'k' : K}
         }
     )
-  
-  return result
+
+    return result
 
 
 def avg_profiles(data, data_classes, K):
-  """
-  Returns some information about the average properties of the profiles
-  mean
-  std
-  returns a list of dictionaries
-  """
+    """
+    Returns some information about the average properties of the profiles
+    mean
+    std
+    returns a list of dictionaries
+    """
 
-  out = []
-  for i in range(K):
-    d = data.where(data_classes==i)
-    d_m = d.mean(('time', 'n'), skipna=True).values
-    d_std = d.std(('time', 'n'), skipna=True, ddof=1).values
-    out.append({'mean' : d_m, 'std' : d_std})
-  return out
+    out = []
+    for i in range(K):
+        d = data.where(data_classes==i)
+        d_m = d.mean(('time', 'n'), skipna=True).values
+        d_std = d.std(('time', 'n'), skipna=True, ddof=1).values
+        out.append({'mean' : d_m, 'std' : d_std})
+    return out
 
 def pca_sort(data_classes, gmm):
-  """
-  Returns a new array of class assignnments, with the classes now ordered by mean value of the first pca component
-  """
-  means = gmm.means_[:, 0]
-  inds = np.argsort(means)
-  return reorder(data_classes, inds)
+    """
+    Returns a new array of class assignnments, with the classes now ordered by mean value of the first pca component
+    """
+    means = gmm.means_[:, 0]
+    inds = np.argsort(means)
+    return reorder(data_classes, inds)
 
 
 def match_profiles(avg0, avg1, dist=None):
-  """
-  Attempt to match profiles of the same shape
-  avg0: list of dict, profile means and std
-  avg1: list of dict, profile means and std
-  dist: dict, dict -> float64 distance between two profiles
-  returns:
-  inds: array of indices that sorts the profiles in avg1 to match avg0
-  """
-  n_classes = len(avg0)
-  assert n_classes == len(avg1)
-  norms = np.zeros((n_classes, n_classes))
-  for i in range(n_classes):
-    for j in range(n_classes):
-      if dist is None:
-        diff = avg0[i]['mean'] - avg1[j]['mean']
-        norms[i, j] = np.sum(diff * diff)
-        #norms[j, i] = norms[i, j]
-      else:
-        norms[i, j] = dist(avg0[i], avg1[j])
-        #norms[j, i] = norms[i, j]
-  return np.argmin(norms, axis=1)
+    """
+    Attempt to match profiles of the same shape
+    avg0: list of dict, profile means and std
+    avg1: list of dict, profile means and std
+    dist: dict, dict -> float64 distance between two profiles
+    returns:
+    inds: array of indices that sorts the profiles in avg1 to match avg0
+    """
+    n_classes = len(avg0)
+    assert n_classes == len(avg1)
+    norms = np.zeros((n_classes, n_classes))
+    for i in range(n_classes):
+        for j in range(n_classes):
+            if dist is None:
+                diff = avg0[i]['mean'] - avg1[j]['mean']
+                norms[i, j] = np.sum(diff * diff)
+                #norms[j, i] = norms[i, j]
+            else:
+                norms[i, j] = dist(avg0[i], avg1[j])
+                #norms[j, i] = norms[i, j]
+    return np.argmin(norms, axis=1)
 
 def match_spatial(data_classes, data_classes_ref, n_classes):
-  """
-  Attempt to match profiles with the same spatial distribution
-  data_classes: 
-  data_classes_ref:
-  n_classes
-  returns:
-  inds: array of indices that sorts the classes in data_classes
-  """
-  
-  counts = np.array([[((data_classes.where(data_classes_ref==k) == j) * np.cos(data_classes['lat'] * 3.1415/180)).sum(skipna=True) for k in range(n_classes) ] for j in range(n_classes)])
-  return np.argmax(np.array(counts), axis=0)
+    """
+    Attempt to match profiles with the same spatial distribution
+    data_classes: 
+    data_classes_ref:
+    n_classes
+    returns:
+    inds: array of indices that sorts the classes in data_classes
+    """
+
+    counts = np.array([[((data_classes.where(data_classes_ref==k) == j) * np.cos(data_classes['lat'] * 3.1415/180)).sum(skipna=True) \
+                        for k in range(n_classes) ] for j in range(n_classes)])
+    return np.argmax(np.array(counts), axis=0)
 
 
 def temp_sort(data_classes, avg, arg=False):
-  """
-  Returns a new array of class assignnments, with the classes now ordered by mean value of the first pca component
-  """
-  top_mean = np.array([i['mean'][0] for i in avg])
-  inds = np.argsort(top_mean)
-  if arg:
-    return inds
-  else:
-    return reorder(data_classes, inds)
-  
+    """
+    Returns a new array of class assignnments, with the classes now ordered by mean value of the first pca component
+    """
+    top_mean = np.array([i['mean'][0] for i in avg])
+    inds = np.argsort(top_mean)
+    if arg:
+        return inds
+    else:
+        return reorder(data_classes, inds)
+
 def reorder(data_classes, inds):
-  inds_r = np.zeros(np.shape(inds))
-  inds_r[inds] = np.arange(0, np.size(inds), dtype='int')
-  
-  def func(arr):
-    out = inds_r[arr.astype('int')]
-    out[arr == -1] = -1
+    inds_r = np.zeros(np.shape(inds))
+    inds_r[inds] = np.arange(0, np.size(inds), dtype='int')
+
+    def func(arr):
+        out = inds_r[arr.astype('int')]
+        out[arr == -1] = -1
     return out
 
-  result = xr.apply_ufunc(
+    result = xr.apply_ufunc(
         func,
         data_classes,
         input_core_dims=[[]],
@@ -469,8 +459,8 @@ def reorder(data_classes, inds):
             'output_sizes' : {}
         }
     )
-  
-  return result
+
+    return result
 
 def modal_classes(data_classes):
   
@@ -489,7 +479,6 @@ def modal_classes(data_classes):
           'output_sizes' : {},
           'allow_rechunk' : True
         }
-        
-    )
+        )
   
   
